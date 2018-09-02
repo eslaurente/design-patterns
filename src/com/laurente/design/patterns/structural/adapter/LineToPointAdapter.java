7ab88f37f8f79c488b842a893376ecd3cad78352
eslaurente <e.s.laurente@gmail.com>
2018-09-02 14:30:52 -0700

    Refactored LineToPointAdapter to implement caching using hashCode and HashMap and implementing Iterable<T>

--- src/com/laurente/design/patterns/structural/adapter/LineToPointAdapter.java
+++ src/com/laurente/design/patterns/structural/adapter/LineToPointAdapter.java
@@ -1,13 +1,23 @@
 package com.laurente.design.patterns.structural.adapter;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.function.Consumer;
 
-public class LineToPointAdapter extends ArrayList<Point> {
+public class LineToPointAdapter implements Iterable<Point> {
 
     private static int count = 0;
+    private static Map<Integer, List<Point>> cache = new HashMap<>();
+    private int hashCode;
 
     public LineToPointAdapter(Line line) {
-        System.out.println((++count) + ": Generating points for line [" + line.start.x + ", " + line.start.y + "] - [" + line.end.x + ", " + line.end.y + "] (no caching)");
+        hashCode = line.hashCode();
+        if (cache.get(hashCode) != null) {
+            return;
+        }
+
+        System.out.println((++count) + ": Generating points for line [" + line.start.x + ", " + line.start.y + "] - [" + line.end.x + ", " + line.end.y + "] (with caching)");
+
+        ArrayList<Point> points = new ArrayList<>();
 
         int left = Math.min(line.start.x, line.end.x);
         int right = Math.max(line.start.x, line.end.x);
@@ -18,13 +28,30 @@ public class LineToPointAdapter extends ArrayList<Point> {
 
         if (dx == 0) {
             for (int y = top; y <= bottom; y++) {
-                add(new Point(left, y));
+                points.add(new Point(left, y));
             }
         }
         else if (dy == 0) {
             for (int x = left; x <= right; x++) {
-                add(new Point(x, top));
+                points.add(new Point(x, top));
             }
         }
+        cache.put(hashCode, points);
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return cache.get(hashCode).iterator();
+    }
+
+    @Override
+    public Spliterator<Point> spliterator() {
+        return cache.get(hashCode).spliterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Point> action) {
+        cache.get(hashCode).forEach(action);
     }
 }
+
