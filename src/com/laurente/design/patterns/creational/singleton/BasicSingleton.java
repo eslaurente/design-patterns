3ba5c982f972829c6ada1acd8b0ea6dce65f855b
eslaurente <e.s.laurente@gmail.com>
2018-09-02 00:09:42 -0700

    Added Serilization to preserver Singleton

--- src/com/laurente/design/patterns/creational/singleton/BasicSingleton.java
+++ src/com/laurente/design/patterns/creational/singleton/BasicSingleton.java
@@ -1,6 +1,8 @@
 package com.laurente.design.patterns.creational.singleton;
 
-public class BasicSingleton {
+import java.io.Serializable;
+
+public class BasicSingleton implements Serializable {
     private static final BasicSingleton INSTANCE = new BasicSingleton();
     private int value = 0;
 
@@ -16,5 +18,10 @@ public class BasicSingleton {
 
     public void setValue(int value) {
         this.value = value;
+    }
+
+    // Ensures that serialization resolves against this instance, preventing copying
+    protected Object readResolve() {
+        return INSTANCE;
     }
 }
--- src/com/laurente/design/patterns/creational/singleton/SingletonDemo.java
+++ src/com/laurente/design/patterns/creational/singleton/SingletonDemo.java
@@ -1,13 +1,43 @@
 package com.laurente.design.patterns.creational.singleton;
 
+import java.io.*;
+import java.nio.file.Paths;
+
 public class SingletonDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         // ** Singleton **
         BasicSingleton singleton = BasicSingleton.getInstance();
         singleton.setValue(123);
         System.out.println(singleton.getValue());
         System.out.println();
 
-        //
+        // Problems with basic Singleton:
+        // 1. Reflection - you can bypass private constructor using Reflection
+        // 2. Serialization - clone object regardless of private constructor
+
+        // Serializing singleton:
+        BasicSingleton singletonToFile = BasicSingleton.getInstance();
+        singletonToFile.setValue(111);
+        String fileName = "singleton.bin";
+        saveToFile(singletonToFile, fileName);
+
+        singleton.setValue(222);
+        BasicSingleton singletonFromFile = readFromFile(fileName);
+        String output = String.format("'singletonToFile' == 'singletonFromFile: %s | singletonToFile.getValue(): %d | singletonFromFile.getValue(): %s'", singletonToFile == singletonFromFile, singletonToFile.getValue(), singletonFromFile.getValue());
+        System.out.println(output);
+    }
+
+    public static void saveToFile(BasicSingleton singleton, String fileName) throws IOException {
+        try (FileOutputStream fileOut = new FileOutputStream(fileName);
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(singleton);
+        }
+    }
+
+    public static BasicSingleton readFromFile(String fileName) throws IOException, ClassNotFoundException {
+        try (FileInputStream fileIn = new FileInputStream(fileName);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            return (BasicSingleton) in.readObject();
+        }
     }
 }
