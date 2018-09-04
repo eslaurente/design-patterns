38a147fe1a21f552cb21f512efd407f4fb5c92f4
eslaurente <e.s.laurente@gmail.com>
2018-09-03 18:44:51 -0700

    Added Composite example using GraphicObject (scalar) is itself composable

---
+++ src/com/laurente/design/patterns/structural/composite/Circle.java
@@ -0,0 +1,9 @@
+package com.laurente.design.patterns.structural.composite;
+
+public class Circle extends GraphicObject {
+
+    public Circle(String color) {
+        name = "Circle";
+        this.color = color;
+    }
+}
---
+++ src/com/laurente/design/patterns/structural/composite/Square.java
@@ -0,0 +1,9 @@
+package com.laurente.design.patterns.structural.composite;
+
+public class Square extends GraphicObject {
+
+    public Square(String color) {
+        name = "Square";
+        this.color = color;
+    }
+}
